12ed4204ea9dd926f36c43df422bdfb23cb295a3
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2024-12-01 16:19:02 +0100

    day 1 silver

---
+++ 1/silver/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let text = std::fs::read_to_string("../input.txt").unwrap();
+
+    let lines = text.split("\n");
+
+    let mut first_vec: Vec<i32> = Vec::new();
+    let mut second_vec: Vec<i32> = Vec::new();
+
+    let mut result: i32 = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            break;
+        }
+
+        let (first, second) = line.split_at(5);
+        let (_, second) = second.split_at(3);
+
+        first_vec.push(first.parse::<i32>().unwrap());
+        second_vec.push(second.parse::<i32>().unwrap());
+    }
+
+    first_vec.sort();
+    second_vec.sort();
+
+    for (idx, first) in first_vec.into_iter().enumerate() {
+        let second = second_vec[idx];
+        result += (first - second).abs();
+    }
+
+    println!("{}", result);
+}
